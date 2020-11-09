fef8c2ed7cf64751866cb816c8218c9b36fbcb8f
zwykły tomek <39235399+tonio965@users.noreply.github.com>
2020-11-09 01:20:16 +0100

    empty spring proj

---
+++ src/main/java/id/MyResourceConfig.java
@@ -0,0 +1,18 @@
+package id;
+
+import java.nio.file.Path;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyResourceConfig extends ResourceConfig{
+
+  public MyResourceConfig() {
+	  
+  }
+  
+
+
+}
