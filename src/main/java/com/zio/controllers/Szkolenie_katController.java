7691691de1f4a87005a96787fb7359664ab535ac
zwykły tomek <39235399+tonio965@users.noreply.github.com>
2020-12-10 14:54:11 +0100

    added szkoleniekat crud2

--- src/main/java/com/zio/controllers/Szkolenie_katController.java
+++ src/main/java/com/zio/controllers/Szkolenie_katController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.transaction.annotation.Transactional;
 import com.zio.exceptions.ItemNotFoundException;
+import com.zio.models.Kandydat;
 import com.zio.models.Szkolenie;
 import com.zio.models.SzkolenieKat;
 import com.zio.repositories.Szkolenie_katRepository;
@@ -43,6 +45,13 @@ public class Szkolenie_katController {
 	@DeleteMapping(value = "/{id}")
 	public void delete(@PathVariable Integer id) {
 	    repository.deleteByIdSzkKat(id);
+	}
+	@Transactional
+	@PutMapping(value="/{id}")
+	public void editSzkolenieKat(@PathVariable Integer id, @RequestBody SzkolenieKat szkolenieKat) {
+		SzkolenieKat p = repository.findByIdSzkKat(id);
+		p.setnazwaSzkKat(szkolenieKat.getnazwaSzkKat());
+		repository.save(p);
 	}
 	
 
--- src/main/java/com/zio/repositories/Szkolenie_katRepository.java
+++ src/main/java/com/zio/repositories/Szkolenie_katRepository.java
@@ -3,6 +3,8 @@ package com.zio.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.zio.models.Szkolenie;
 import com.zio.models.SzkolenieKat;
@@ -11,5 +13,6 @@ public interface Szkolenie_katRepository extends JpaRepository<SzkolenieKat, Int
 	
 	SzkolenieKat findByIdSzkKat(int id_szk_kat);
 	void deleteByIdSzkKat(int id_szk_kat);
+	
 
 }
