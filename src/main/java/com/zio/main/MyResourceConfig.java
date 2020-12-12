305469c550377e8133b309a7c0816fe853f87e22
tonio965 <tonio96@o2.pl>
2020-12-12 18:44:38 +0100

    cos tam dziala najwyzej se cofne 3 komity

--- src/main/java/com/zio/main/MyResourceConfig.java
+++ src/main/java/com/zio/main/MyResourceConfig.java
@@ -3,16 +3,64 @@ package com.zio.main;
 import java.nio.file.Path;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.stereotype.Component;
+
+import com.zio.repositories.PracownikHrRepository;
 
 
 //@Component
-public class MyResourceConfig extends ResourceConfig{
+//@Configuration
+//@EnableAuthorizationServer
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class MyResourceConfig extends WebSecurityConfigurerAdapter{
 
-  public MyResourceConfig() {
-	  System.out.println("config test");
-  }
-  
 
+	@Autowired
+	PracownikHrRepository repository;
+	
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().authorizeRequests().antMatchers("/oauth/token")
+		.permitAll().anyRequest().authenticated();
+		
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		return provider;
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(authenticationProvider());
+	}
 
 }
